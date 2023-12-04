2028b4ae86359eec972d71b8232f9b14411cc498
deathzombee <arfarfwegotcha@gmail.com>
2023-12-04 00:13:07 -0800

    part2

--- day2/main.go
+++ day2/main.go
@@ -16,6 +16,7 @@ func main() {
 
 	redLimit, greenLimit, blueLimit := 12, 13, 14
 	var sumOfGameIDs int
+	var sumOfGamePowers int
 
 	for _, line := range lines {
 		possible, _ := isGamePossible(line, redLimit, greenLimit, blueLimit)
@@ -24,9 +25,13 @@ func main() {
 			sumOfGameIDs += gameID
 			//fmt.Printf("Game %d is possible with %d subsets\n", gameID, subsetCount)
 		}
+		power := gamePower(line)
+		sumOfGamePowers += power
+		//fmt.Printf("Game %d has power %d\n", getGameID(line), power)
 	}
 
 	fmt.Printf("Sum of game IDs: %d\n", sumOfGameIDs)
+	fmt.Printf("Sum of game powers: %d\n", sumOfGamePowers)
 }
 
 func getGameID(line string) int {
@@ -67,4 +72,42 @@ func isGamePossible(line string, redLimit, greenLimit, blueLimit int) (bool, int
 		}
 	}
 	return true, len(subsets)
+}
+func gamePower(line string) int {
+	var redCount, greenCount, blueCount int
+	parts := strings.Split(line, ": ")
+	subsets := strings.Split(parts[1], ";")
+	for _, subset := range subsets {
+		cubes := strings.Split(strings.TrimSpace(subset), ",")
+		for _, cube := range cubes {
+			colorCount := strings.Split(strings.TrimSpace(cube), " ")
+			count, err := strconv.Atoi(colorCount[0])
+
+			if err != nil {
+				log.Fatalf("gamePower: %v", err)
+			}
+
+			//keep track of max count of each color in each game
+			switch colorCount[1] {
+			case "red":
+				if count > redCount {
+					redCount = count
+				}
+			case "green":
+				if count > greenCount {
+					greenCount = count
+				}
+			case "blue":
+				if count > blueCount {
+					blueCount = count
+				}
+			}
+
+			//fmt.Printf("colorCount: %v\n", colorCount)
+
+		}
+
+	}
+
+	return redCount * greenCount * blueCount
 }
