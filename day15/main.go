8dd7e55d3b4a411365cee242ad2f0f7eafe73035
deathzombee <arfarfwegotcha@gmail.com>
2023-12-17 10:00:45 -0800

    day 15 part 2

--- day15/main.go
+++ day15/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -26,16 +27,79 @@ func hashList(s string) int32 {
 	list := strings.Split(s, ",")
 	for _, v := range list {
 		hashValue := hash(v)
-		//fmt.Printf("%s: %d\n", v, hashValue)
 		added += hashValue
 	}
 	return added
 }
+func hashmap(s string) map[int32][]string {
+	m := make(map[int32][]string)
+	list := strings.Split(s, ",")
 
+	for _, v := range list {
+		label := v
+		value := ""
+
+		if idx := strings.Index(v, "="); idx != -1 {
+			label = v[:idx]
+			value = v[idx+1:]
+			hashValue := hash(label)
+			found := false
+			for i, val := range m[hashValue] {
+				if strings.HasPrefix(val, label+" ") {
+					// Replace existing lens
+					m[hashValue][i] = label + " " + value
+					found = true
+					break
+				}
+			}
+			if !found {
+				// Add new lens
+				m[hashValue] = append(m[hashValue], label+" "+value)
+			}
+		} else if idx := strings.Index(v, "-"); idx != -1 {
+			label = v[:idx]
+			hashValue := hash(label)
+			// Remove the lens and shift others forward
+			var newBox []string
+			for _, val := range m[hashValue] {
+				if !strings.HasPrefix(val, label+" ") {
+					newBox = append(newBox, val)
+				}
+			}
+			m[hashValue] = newBox
+		}
+	}
+	return m
+}
+func focusingPower(m map[int32][]string) int {
+	totalPower := 0
+
+	for box, lenses := range m {
+		for i, lens := range lenses {
+			parts := strings.Fields(lens)
+			if len(parts) < 2 {
+				continue // Skip if the lens does not have a focal length
+			}
+			focalLength, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Println("Error converting focal length:", parts[1])
+				continue
+			}
+			power := (int(box) + 1) * (i + 1) * focalLength
+			totalPower += power
+		}
+	}
+	return totalPower
+}
 func main() {
 	intructions := input
 	t1 := time.Now()
 	p1 := hashList(intructions)
 	fmt.Println("p1 time:", time.Since(t1))
 	fmt.Println("p1:", p1)
+	t2 := time.Now()
+	p2 := hashmap(intructions)
+	totalPower := focusingPower(p2)
+	fmt.Println("p2 time:", time.Since(t2))
+	fmt.Println("Total Focusing Power:", totalPower)
 }
