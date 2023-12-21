b7abeedab02f2f1e5cb26045fb88c4df98b7513b
deathzombee <arfarfwegotcha@gmail.com>
2023-12-20 16:23:30 -0800

    if the input is wrong the output will be wrong

--- day18/main.go
+++ day18/main.go
@@ -1,75 +1,95 @@
 package main
 
 import (
+	"AoC2023/utils"
 	"fmt"
+	"io"
 	"math"
 	"strings"
 	"time"
 )
 
-type Point struct {
+type point struct {
 	X, Y int
 }
 
-type Interval struct {
-	Start, End Point
+type interval struct {
+	Start, End point
 }
 
-func processInstructions(instructions string) ([]Interval, []Point, int, int, int, int) {
-	var allIntervals []Interval
+// process input return a string with directions and steps, and a slice of comma enclosed color codes
+func processInput(input []string) (string, []string) {
+	var directions string
+	var colors []string
+	for _, line := range input {
+		parts := strings.Split(line, ",")
+		for _, part := range parts {
+			part = strings.TrimSpace(part)
+			if part[0] == 'R' || part[0] == 'L' || part[0] == 'U' || part[0] == 'D' {
+				directions += part + ", "
+			} else {
+				colors = append(colors, part)
+			}
+		}
+	}
+	return directions, colors
+}
+func processInstructions(instructions string) ([]interval, []point) {
+	var allIntervals []interval
 	x, y := 0, 0 // Starting point
-	points := []Point{{0, 0}}
-	current := Point{0, 0}
-	minX, minY, maxX, maxY := 0, 0, 0, 0
+	points := []point{{0, 0}}
+	current := point{0, 0}
 
 	for _, instruction := range strings.Split(instructions, ", ") {
 		var dir string
 		var steps int
-		fmt.Sscanf(instruction, "%s %d", &dir, &steps)
+
+		n, err := fmt.Sscanf(instruction, "%s %d", &dir, &steps)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error parsing instruction: %v\n", err)
+			continue // Skip this instruction and continue with the next one
+		}
+		if n != 2 {
+			fmt.Printf("Invalid instruction format: %s\n", instruction)
+			continue
+		}
 
 		switch dir {
 		case "R":
-			newInterval := Interval{Start: Point{x, y}, End: Point{x + steps, y}}
+			newInterval := interval{Start: point{x, y}, End: point{x + steps, y}}
 			allIntervals = append(allIntervals, newInterval)
 			x += steps
-			current = Point{x, y}
-			if x > maxX {
-				maxX = x
-			}
+
+			current = point{x, y}
 		case "L":
-			newInterval := Interval{Start: Point{x, y}, End: Point{x - steps, y}}
+			newInterval := interval{Start: point{x, y}, End: point{x - steps, y}}
 			allIntervals = append(allIntervals, newInterval)
 			x -= steps
-			current = Point{x, y}
-			if x < minX {
-				minX = x
-			}
+			current = point{x, y}
 		case "U":
 			ynew := y - steps
-			newInterval := Interval{Start: Point{x, y}, End: Point{x, ynew}}
+			newInterval := interval{Start: point{x, y}, End: point{x, ynew}}
 			allIntervals = append(allIntervals, newInterval)
 			y = ynew
-			current = Point{x, y}
-			if ynew < minY {
-				minY = ynew
-			}
+			current = point{x, y}
 		case "D":
 			ynew := y + steps
-			newInterval := Interval{Start: Point{x, y}, End: Point{x, ynew}}
+			newInterval := interval{Start: point{x, y}, End: point{x, ynew}}
 			allIntervals = append(allIntervals, newInterval)
 			y = ynew
-			current = Point{x, y}
-			if ynew > maxY {
-				maxY = ynew
-			}
+			current = point{x, y}
 		}
+
 		points = append(points, current)
 	}
 
-	return allIntervals, points, minX, minY, maxX, maxY
+	return allIntervals, points
 }
 
-func calculatePathPerimeter(points []Point) int {
+func calculatePathPerimeter(points []point) int {
 	perimeter := 0
 	for i := 0; i < len(points)-1; i++ {
 		perimeter += distance(points[i], points[i+1])
@@ -77,11 +97,8 @@ func calculatePathPerimeter(points []Point) int {
 	return perimeter
 }
 
-func distance(a, b Point) int {
-	return int(math.Abs(float64(a.X-b.X)) + math.Abs(float64(a.Y-b.Y)))
-}
-func calculateArea(intervals []Interval) float64 {
-	var vertices []Point
+func calculateArea(intervals []interval) float64 {
+	var vertices []point
 	for _, interval := range intervals {
 		if len(vertices) == 0 || vertices[len(vertices)-1] != interval.Start {
 			vertices = append(vertices, interval.Start)
@@ -103,19 +120,23 @@ func calculateArea(intervals []Interval) float64 {
 
 	return math.Abs(area) / 2
 }
-func main() {
-	// Example instructions
-	instructions := "R 6, D 5, L 2, D 2, R 2, D 2, L 5, U 2, L 1, U 2, R 2, U 3, L 2, U 2"
-	t := time.Now()
-	allIntervals, points, _, _, _, _ := processInstructions(instructions)
+
+func distance(a, b point) int {
+	return int(math.Abs(float64(a.X-b.X)) + math.Abs(float64(a.Y-b.Y)))
+}
+
+func part1(input string) int {
+	allIntervals, points := processInstructions(input)
 	per := calculatePathPerimeter(points)
-	fmt.Println("Run time:", time.Since(t))
-	fmt.Printf("Perimeter of the path is: %d\n", per)
-	t2 := time.Now()
 	area := calculateArea(allIntervals)
-	total := per/2 + int(area) + 1
-	fmt.Println("Run time:", time.Since(t2))
-	fmt.Printf("Total is: %d\n", total)
-	inside := total - per
-	fmt.Printf("Filled is: %d\n", inside)
+	picksArea := per/2 + int(area) + 1
+	return picksArea
+}
+func main() {
+	input, _ := utils.ReadLines("input.txt")
+	instr, _ := processInput(input)
+	t := time.Now()
+	p1 := part1(instr)
+	fmt.Println("Part 1:", time.Since(t))
+	fmt.Println("Area:", p1)
 }
